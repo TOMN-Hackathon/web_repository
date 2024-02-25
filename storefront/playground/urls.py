6912e05ba571c574d35ab0e920c5a0b02b6a2ebb
Nathan Truong <tnathan1388@gmail.com>
2024-02-25 16:09:22 +0000

    Created Static Directory & Json Files for Msg History

--- storefront/playground/urls.py
+++ storefront/playground/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
@@ -6,4 +8,4 @@ urlpatterns = [
     path("user/", views.user),
     path("messages/", views.messages),
     path("ai/", views.ai)
-]
+] + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -1,14 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import json
 
 # Create your views here.
 def landing(request):
     #messages_dict = file.open()
     return render(request, "index.html")
 
+def add_to_json(user, message):
+    with open('thing.json', encoding='utf-8') as data_file:
+        data = json.loads(data_file.read())
+    data_file.close()
+    print(data)
+    print(type(data))
+    data.append({"role":user, "message" :message})
+    print(data)
+    with open('thing.json', 'w') as data_file:
+        json.dump(data, data_file)
+    data_file.close()
+
+
+
 def user(request):
-	userMessage= request.GET.get("userMessage", "")
-	return HttpResponse(f"<span class='user-message'><p>{userMessage}</p></span>", content_type="text/html")
+    userMessage = request.GET.get("userMessage", "")
+    add_to_json("user", userMessage)
+    return HttpResponse(f"<span class='user-message'><p>{userMessage}</p></span>", content_type="text/html")
 
 def messages(request):
     pass
