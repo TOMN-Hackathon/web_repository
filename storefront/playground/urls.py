17fe7a24f8436087cd4b6d3871e1b23a6a3fada1
Nathan Truong <tnathan1388@gmail.com>
2024-02-25 13:22:30 +0000

    added endpoints and functions

--- storefront/playground/urls.py
+++ storefront/playground/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path("home/",views.landing)
+    path("user/", view.user)
+    path("messages/", view.messages)
+    path("ai/", view.ai)
 ]
--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -3,4 +3,14 @@ from django.http import HttpResponse
 
 # Create your views here.
 def landing(request):
+    messages_dict = file.open()
     return HttpResponse("<p>Hello World</p>",content_type="text/html")
+
+def user(request):
+    pass
+
+def messages(request):
+    pass 
+
+def ai(request):
+    pass
