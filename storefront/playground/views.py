6a8b5e333ab4fba852023de0686030f54335925f
Nathan Truong <tnathan1388@gmail.com>
2024-02-25 17:05:55 +0000

    Fixed views.py path reference to message.json

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -8,14 +8,14 @@ def landing(request):
     return render(request, "index.html")
 
 def add_to_json(user, message):
-    with open('thing.json', encoding='utf-8') as data_file:
+    with open('storefront/playground/templates/message.json', encoding='utf-8') as data_file:
         data = json.loads(data_file.read())
     data_file.close()
     print(data)
     print(type(data))
     data.append({"role":user, "message" :message})
     print(data)
-    with open('thing.json', 'w') as data_file:
+    with open('storefront/playground/templates/message.json', 'w') as data_file:
         json.dump(data, data_file)
     data_file.close()
 
@@ -27,7 +27,12 @@ def user(request):
     return HttpResponse(f"<span class='user-message'><p>{userMessage}</p></span>", content_type="text/html")
 
 def messages(request):
-    pass
+    with open('storefront/playground/templates/message.json', encoding='utf-8') as data_file:
+        data = json.loads(data_file.read())
+    data_file.close()
+    json_d_data = json.dumps(data)
+    return json_d_data
+
 
 def ai(request):
     pass
